3d66c4ffa515ddd8a9957f0c44f1e005d690ccab
trossossp3 <21trossos@gmail.com>
2022-04-09 00:57:01 -0400

    did some search stuff

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Hashids = require("hashids/cjs");
 const { redirect } = require("express/lib/response");
 const { is } = require("express/lib/request");
 const hashids = new Hashids();
-
+const Schema = mongoose.Schema;
 // console.log(`${process.env.PSW}`);
 var port = process.env.PORT || 3000;
 app.use(express.static("css"));
@@ -21,12 +21,14 @@ mongoose.connect(
   `mongodb+srv://trossos:test@cluster0.gkfih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&authSource=admin`
 );
 
-const schema = {
+var schema = new Schema({
   food_item: String,
   location_from: String,
   mass: String,
-  product_code: String,
-};
+  product_code: String
+});
+
+schema.index({location_from: "text"});
 
 const foods = mongoose.model("foods", schema);
 
@@ -35,7 +37,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/index", function (req, res) {
-  res.render(__dirname + "/index.ejs", {is_admin: is_admin});
+  res.render(__dirname + "/index.ejs", { is_admin: is_admin });
 });
 
 app.get("/enter-code", function (req, res) {
@@ -51,7 +53,7 @@ app.get("/bad-input", function (req, res) {
 
 app.get("/edit", function (req, res) {
   foods.find({ product_code: cur_code }, function (err, data) {
-    console.log("data:" + data);
+    // console.log("data:" + data);
     res.render(__dirname + "/edit.ejs", { foods: data });
   });
 
@@ -60,7 +62,7 @@ app.get("/edit", function (req, res) {
 
 app.get("/display-code", function (req, res) {
   foods.find({ product_code: cur_code }, function (err, data) {
-    console.log("data:" + data);
+    // console.log("data:" + data);
     res.render(__dirname + "/display-code.ejs", { foods: data });
   });
 });
@@ -68,7 +70,7 @@ app.get("/display-code", function (req, res) {
 app.get("/jagger", function (req, res) {
   foods.find({}, function (err, data) {
     // note that data is an array of objects, not a single object!
-    console.log(data);
+    // console.log(data);
     res.render(__dirname + "/jagger.ejs", {
       foods: data,
     });
@@ -87,17 +89,17 @@ app.post("/add-new", async (req, res) => {
     location_from: req.body.location_from,
     mass: req.body.mass,
   });
-  console.log("\n\n\n\n");
+  //console.log("\n\n\n\n");
   var idStr = fix_id(test._id.valueOf());
-  console.log(typeof idStr);
+  //console.log(typeof idStr);
   var idInt = hashids.encode(BigInt(idStr));
-  console.log(hashids.encode(idInt));
+  //console.log(hashids.encode(idInt));
   test.product_code = idInt;
   cur_code = test.product_code;
-  console.log(test.product_code);
-  console.log(test);
+  //console.log(test.product_code);
+  //console.log(test);
   test.save(function (err, doc) {
-    if (err) return console.error(err);
+    if (err) return //console.error(err);
     console.log("Document inserted succussfully!");
   });
   res.redirect("/display-code");
@@ -106,9 +108,9 @@ app.post("/add-new", async (req, res) => {
 app.post("/edit", async (req, res) => {
   console.log("editing POST");
   const temp = await foods.findOne({ product_code: cur_code });
-  console.log("curr element" + temp);
+  // console.log("curr element" + temp);
   const filter = { product_code: cur_code };
-  console.log(filter);
+  //console.log(filter);
 
   // console.log(update);
   const opts = { new: true };
@@ -123,20 +125,27 @@ app.post("/edit", async (req, res) => {
 
   res.redirect("/index");
 });
+app.post("/search", async (req, res) => {
+  // const resa = await foods.index({food_item: "text"});
+  var results = foods.find({ $text: { $search: "\"Bread\"" } });
+  const docs = await results;
+  console.log(docs);
+  
+});
 
 app.post("/exsists", function (req, res) {
-  console.log("test");
+  //console.log("test");
 
   foods.exists({ product_code: req.body.code }, function (err, result) {
     if (err) {
-      console.log(err);
+      //console.log(err);
     } else {
-      console.log(result);
+      //console.log(result);
       if (result == null) {
         res.redirect("/bad-input");
       } else {
         cur_code = req.body.code;
-        console.log(cur_code);
+        //console.log(cur_code);
         res.redirect("/edit");
       }
     }
@@ -145,18 +154,16 @@ app.post("/exsists", function (req, res) {
 
 app.post("/volunteer", async (req, res) => {
   is_admin = false;
-  console.log(is_admin);
-  res.redirect("/index")
-
+  //console.log(is_admin);
+  res.redirect("/index");
 });
 app.post("/admin", async (req, res) => {
   is_admin = true;
-  console.log(is_admin);
-  res.redirect("/index")
-
+  //console.log(is_admin);
+  res.redirect("/index");
 });
 function fix_id(id) {
-  console.log(id);
+  //console.log(id);
   var out = "";
   for (var i = 0; i < id.length; i++) {
     if (id.charCodeAt(i) < 58) {
@@ -165,7 +172,7 @@ function fix_id(id) {
       out += id.charCodeAt(i);
     }
   }
-  console.log(out);
+  //console.log(out);
   return out;
 }
 
